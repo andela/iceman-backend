8e1a4bc35fc3bc7797fc02c31e253e57f5de02fc
cleave3 <owhiroroeghele@gmail.com>
2019-08-14 19:54:31 +0100

    Setup unit testing for created app functionalities

---
+++ src/test/index.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('API ROUTES', () => {
+  it('should get all entry points', async () => {
+    const res = await chai.request(app)
+      .get('/');
+    res.should.have.status(200);
+  });
+  it('should return a not found error message when an invalid route is accessed', async () => {
+    const res = await chai.request(app)
+      .get('/fakepath');
+    res.should.have.status(404);
+  });
+});
