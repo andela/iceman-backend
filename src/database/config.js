7a5d19a09cb363ecff611dc2e3b5260e01a2639b
Evuazeze Emmanuel Oghenewegba <evuazeze.emmanuel@gmail.com>
2019-08-15 11:26:27 +0100

    chore:setup postgresql for production and development

---
+++ src/database/config.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+const secret = require("../config").secret;
+
+let pool;
+
+if (process.env.NODE_ENV === "production") {
+  pool = new Pool({
+    connectionString: secret.SECRET
+  });
+}
+else {
+  pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT
+  });
+}
+
+pool.connect();
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  },
+}
