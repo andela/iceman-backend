e4598ce3768d5fec188732fec36d14df1c2e7689
Sam <samuelkoroh@gmail.com>
2019-08-26 16:18:09 +0100

    ft: user can login with facebook and google

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,27 +1,53 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const mongoose = require('mongoose');
+import passport from 'passport';
+// import LocalStrategy from 'passport-local';
+import { User } from '../models';
 
-const User = mongoose.model('User');
+// const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'user[email]',
-      passwordField: 'user[password]'
-    },
-    ((email, password, done) => {
-      User.findOne({ email })
-        .then((user) => {
-          if (!user || !user.validPassword(password)) {
-            return done(null, false, {
-              errors: { 'email or password': 'is invalid' }
-            });
-          }
+const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET } = process.env;
 
-          return done(null, user);
-        })
-        .catch(done);
-    })
-  )
-);
+// passport.use(new GoogleStrategy({
+//   consumerKey: GOOGLE_CONSUMER_KEY,
+//   consumerSecret: GOOGLE_CONSUMER_SECRET,
+//   callbackURL: "http://www.example.com/auth/google/callback"
+//   }, (token, tokenSecret, profile, done) => {
+//       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//         return done(err, user);
+//       });
+//   }
+// ));
+
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: 'http://www.example.com/auth/facebook/callback'
+},
+(accessToken, refreshToken, profile, done) => {
+  User.findOrCreate(profile, (err, user) => {
+    if (err) { return done(err); }
+    done(null, user);
+  });
+}));
+
+// passport.use(
+//   new LocalStrategy(
+//     {
+//       usernameField: 'user[email]',
+//       passwordField: 'user[password]'
+//     },
+//     ((email, password, done) => {
+//       User.findOne({ email })
+//         .then((user) => {
+//           if (!user || !user.validPassword(password)) {
+//             return done(null, false, {
+//               errors: { 'email or password': 'is invalid' }
+//             });
+//           }
+
+//           return done(null, user);
+//         })
+//         .catch(done);
+//     })
+//   )
+// );
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import passport from 'passport';
 import AuthController from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/login', AuthController.loginUser);
+// router.post('/google', AuthController.googleLogIn);
+router.get('/facebook', passport.authenticate('facebook'));
+router.get('/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
 
 export default router;
