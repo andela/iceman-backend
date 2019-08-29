402488d228918c60f71ec4e653d7039454383207
cleave3 <owhiroroeghele@gmail.com>
2019-08-29 15:54:38 +0100

    fix codeclide duplication error

--- src/seeders/20190828170312-demo-request.js
+++ src/seeders/20190828170312-demo-request.js
@@ -1,33 +1,23 @@
 
+const demoRequest = id => ({
+  id,
+  source: 'lagos',
+  destination: 'newyork',
+  travelDate: new Date(),
+  returnDate: new Date(),
+  tripType: 'one-way',
+  reason: 'business',
+  accommodation: 'Lorem ipsum',
+  status: 'pending',
+  userId: 1,
+  createdAt: new Date(),
+  updatedAt: new Date()
+});
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Requests', [{
-    id: 1,
-    source: 'lagos',
-    destination: 'newyork',
-    travelDate: new Date(),
-    returnDate: new Date(),
-    tripType: 'one-way',
-    reason: 'business',
-    accommodation: 'Lorem ipsum',
-    status: 'pending',
-    userId: 1,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: 2,
-    source: 'lagos',
-    destination: 'nairobi',
-    travelDate: new Date(),
-    returnDate: new Date(),
-    tripType: 'return',
-    reason: 'business',
-    accommodation: 'Lorem ipsum',
-    status: 'accepted',
-    userId: 1,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }], {}),
+  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Requests', [
+    demoRequest(1),
+    demoRequest(2)
+  ], {}),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Requests', null, {})
 };
