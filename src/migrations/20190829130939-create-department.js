c01f88343242126cc881c1a4f8c9ce21bf8ec4bb
SamuelKoroh <47538882+SamuelKoroh@users.noreply.github.com>
2019-08-30 00:11:36 +0100

    fixing merge conflict

--- src/migrations/20190829130939-create-department.js
+++ src/migrations/20190829130939-create-department.js
@@ -26,5 +26,5 @@ module.exports = {
       type: Sequelize.DATE
     }
   }),
-  down: (queryInterface) => queryInterface.dropTable('Departments')
+  down: queryInterface => queryInterface.dropTable('Departments')
 };
