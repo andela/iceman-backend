66a59a2ee80cc133880252515e4cdec26982e96e
Sam <samuelkoroh@gmail.com>
2019-09-12 17:50:10 +0100

    resolving merge conflict

--- src/routes/request.js
+++ src/routes/request.js
@@ -21,7 +21,7 @@ router.post('/multi-city', [auth, validator(requestSchema)], multiCityRequest);
 router.post('/one-way', [auth, validator(requestSchema)], oneWay);
 router.patch('/:requestId', [auth, validator(requestSchema)], update);
 router.get('/pending', auth, permitUser(['manager']), availOpenRequests);
-router.get('/userRequests', auth, getRequests);
+router.get('/', auth, getRequests);
 router.post('/return', [auth, validator(requestSchema)], returnRequest);
 router.get('/search', auth, search);
 
